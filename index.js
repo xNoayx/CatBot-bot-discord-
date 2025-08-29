db38fc5cff78c441d9df38bae6a532f558b206ad
xNoayx <noadupire190@gmail.com>
2025-08-29 08:49:48 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("fs");
+
+// Client Discord
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+// Commandes
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+// Événements
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+// Login
+client.login(process.env.DISCORD_TOKEN);
